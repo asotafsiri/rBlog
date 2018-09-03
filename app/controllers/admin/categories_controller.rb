e50f093900d6111f75b7e37aede4dbac161dade9
asotafsiri <aso.tafsiri@gmail.com>
2018-09-03 16:36:51 +0430

    handeling insert data to category table

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -18,11 +18,22 @@ class Admin::CategoriesController < Admin::ApplicationController
   end
 
   def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = 'Category Created'
+      redirect_to admin_categories_path
+    else
+      render 'new'
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+  private
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
