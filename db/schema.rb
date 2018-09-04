3ed6991896a2e3a344558aef7d375fc466fd61cd
asotafsiri <aso.tafsiri@gmail.com>
2018-09-05 00:49:21 +0430

    yremoving column image from posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_04_091134) do
+ActiveRecord::Schema.define(version: 2018_09_04_201835) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
