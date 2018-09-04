13dd3d6066ca2550831fd392c06e89f7b1b8e415
asotafsiri <aso.tafsiri@gmail.com>
2018-09-05 01:51:32 +0430

    handeling image loading process

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,6 +9,10 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :category_id, presence: true
   validates :body, presence: true
+
+ # has_one_attached :image, :default_url => ":style/rails1.jpg"
+   has_attached_file :image, :default_url => ":style/rails1.jpg"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\z/
   def self.search(query)
     where("title like ? OR body like ?", "%#{query}%", "%#{query}%")
   end
