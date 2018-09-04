2cde1d113ac0c4f0a9d75385c64a8d6f5f75a50b
asotafsiri <aso.tafsiri@gmail.com>
2018-09-05 01:02:42 +0430

    create field image with paperclip attributes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_04_202622) do
+ActiveRecord::Schema.define(version: 2018_09_04_203112) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -34,6 +34,10 @@ ActiveRecord::Schema.define(version: 2018_09_04_202622) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
