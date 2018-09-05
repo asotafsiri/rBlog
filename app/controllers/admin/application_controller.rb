01ac81bea69c4a5fa738b1981974f5872bda31db
asotafsiri <aso.tafsiri@gmail.com>
2018-09-05 15:51:09 +0430

    handeling login process

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -3,4 +3,14 @@
 # Admin controller
 class Admin::ApplicationController < ActionController::Base
   layout 'admin/application'
+  helper_method :current_user
+  private
+  def current_user
+    @current_user ||=User.find(session[:user_id]) if session[:user_id]
+  end
+  def verify_logged_in
+    unless current_user
+      redirect_to admin_login_path
+    end
+  end
 end
--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -5,5 +5,14 @@ class Admin::SessionsController < Admin::ApplicationController
   layout 'admin/login'
   def new; end
 
-  def create; end
+  def create
+    user = User.authenticate(params[:email], params[:password])
+    if user
+      session[:user_id] = user.id
+      redirect_to admin_posts_path, :notice => "Logged In"
+    else
+      flash.now.alert = "Invalid email password"
+      render 'new'
+    end
+  end
 end
