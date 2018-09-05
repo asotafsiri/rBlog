f15293fdefd4523369ddb94768cef96ad0e51293
asotafsiri <aso.tafsiri@gmail.com>
2018-09-05 13:36:28 +0430

    adding comments to posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,5 +2,15 @@
 
 # class commentscontroller
 class CommentsController < ApplicationController
-  def new; end
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+
+    flash[:notice] = 'Comment Added'
+    redirect_to post_path(@post)
+  end
+ private
+ def comment_params
+  params.require(:comment).permit(:name, :email, :body, :post_id)
+ end
 end
